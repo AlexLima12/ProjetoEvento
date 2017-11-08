b400e319414d38dc6a7c2791702a6f7757a7f858
ALEX PEREIRA DE LIMA <40344056864@jpmorgan.sp>
2017-11-08 21:59:01 -0200

    Fim da Aula - 08/11

---
+++ ClassePai/ClassesFilhas/Show.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace ProjetoEvento.ClassePai.ClassesFilhas
+{
+    public class Show : Evento
+    {
+        public string Artista { get; set; }
+
+        public string GeneroMusical { get; set; }
+
+        public Show()
+        {
+        }
+
+        public Show(string titulo, string local, int lotacao, string duracao, int classificacao, DateTime data, string artista, string generomusical)
+        {
+            base.Titulo = titulo;
+            base.Local = local;
+            base.Lotacao = lotacao;
+            base.Duracao = duracao;
+            base.Classificacao = classificacao;
+            base.Data = data;
+
+            //Argumento a seguir estão na classe filha, por isso usamos o "this"
+            this.Artista = artista;
+            this.GeneroMusical = generomusical;
+        }
+
+        public override bool Cadastrar(){
+            return false;
+        }
+
+    }
+}
